313de6b543716081b3ec1e84a5c27e8d971221c8
Mara K <mariia.kucheieva@gmail.com>
2024-12-05 16:18:50 +0100

    125. Valid Palindrom

---
+++ 125.valid-palindrome.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    while (!isAlphanumeric(s[left])) {
+      left++;
+    }
+
+    while (!isAlphanumeric(s[right])) {
+      right--;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+};
+
+function isAlphanumeric(char) {
+  return (
+    (char >= "A" && char <= "Z") ||
+    (char >= "a" && char <= "z") ||
+    (char >= "0" && char <= "9")
+  );
+}
